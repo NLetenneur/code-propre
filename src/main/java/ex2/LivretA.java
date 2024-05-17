2cf1b908e9bd93df5cf619332ec916f2219b0d1f
Noémie Letenneur <n.letenneur@gmail.com>
2024-05-17 12:22:30 +0200

    TP 01Nettoyage ex2

--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -1,9 +1,24 @@
 package ex2;
 
 public class LivretA extends CompteBancaire {
+	/** tauxRemuneration : taux de rémunération */
+	double tauxRemuneration;
 
-	public LivretA(String type, double solde, double tauxRemuneration) {
-		super(type, solde, 0, tauxRemuneration);
+	/** Constructor
+	 * @param solde
+	 * @param decouvert
+	 * @param tauxRemuneration
+	 */
+	public LivretA(double solde, double tauxRemuneration) {
+		super(solde, 0);
+		this.tauxRemuneration = tauxRemuneration;
 	}
+
+
+	public void appliquerRemuAnnuelle(){
+			double newSolde = this.getSolde()*1+tauxRemuneration/100;
+			this.setSolde(newSolde);
+		}
+
 
 }
