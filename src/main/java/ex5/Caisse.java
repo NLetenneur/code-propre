1d4c77d01ebdfe8d4f60bfb64488a180927f38b8
Noémie Letenneur <n.letenneur@gmail.com>
2024-05-17 15:41:40 +0200

    TP Nettoyage ex 5

--- src/main/java/ex5/Caisse.java
+++ src/main/java/ex5/Caisse.java
@@ -6,14 +6,17 @@ import java.util.List;
 public class Caisse {
 
 	private String nom;
+	private int weightMin;
+	private int weightMax;
 	private List<Item> items;
 
 	/** Constructeur
 	 * @param nom
 	 */
-	public Caisse(String nom) {
-		super();
+	public Caisse(String nom, int weightMin, int weightMax) {
 		this.nom = nom;
+		this.weightMin =weightMin;
+		this.weightMax =weightMax;	
 		this.items = new ArrayList<>();
 	}
 
@@ -43,6 +46,34 @@ public class Caisse {
 	 */
 	public void setItems(List<Item> items) {
 		this.items = items;
+	}
+
+	/** Getter pour weightMin
+	 * @return weightMin
+	 */
+	public int getWeightMin() {
+		return weightMin;
+	}
+
+	/**Setter pour weightMin
+	 * @param weightMin weightMin 
+	 */
+	public void setWeightMin(int weightMin) {
+		this.weightMin = weightMin;
+	}
+
+	/** Getter pour weightMax
+	 * @return weightMax
+	 */
+	public int getWeightMax() {
+		return weightMax;
+	}
+
+	/**Setter pour weightMax
+	 * @param weightMax weightMax 
+	 */
+	public void setWeightMax(int weightMax) {
+		this.weightMax = weightMax;
 	}
 	
 }
